a0406384bda0b64bd5ea2e763f684603820af50f
Jonas Day <jonas.day@niji.fr>
2023-12-30 23:58:02 +0100

    portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,6 +1,6 @@
 import { useRef } from "react"
 import "./Portfolio.scss"
-import { motion, useScroll, useSpring } from "framer-motion"
+import { motion, useScroll, useSpring, useTransform } from "framer-motion"
 
 const items = [
     {
@@ -30,9 +30,30 @@ const items = [
 ]
 
     const Single = ({item}) => {
+
+        const ref = useRef()
+
+        const {scrollYProgress} = useScroll({
+            target: ref,
+            //offset: ["start start", "end start"]
+        })
+
+        const y = useTransform(scrollYProgress, [0, 1], [-300, 300])
+
         return (
         <section>
-            {item.title}
+            <div className="container">
+                <div className="wrapper">
+                    <div className="imageContainer" ref={ref}>
+                        <img src={item.img} alt="" />
+                    </div>
+                    <motion.div className="textContainer" style={{y}}>
+                        <h2>{item.title}</h2>
+                        <p>{item.description}</p>
+                        <button>See Demo</button>
+                    </motion.div>
+                </div>
+            </div>
         </section>)
     }
 
