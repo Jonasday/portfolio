980ef2eb80e5de5b98901b3488e1141434de5359
Jonasday_ <jonas35310@hotmail.fr>
2023-12-30 19:27:42 +0100

    Parallax

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.scss"
-import { Navbar } from './components/navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
 import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
+import Portfolio from "./components/portfolio/Portfolio";
 /* import { Test } from "./Test"; */
 
 function App () {
@@ -12,12 +14,10 @@ function App () {
       <Navbar />
       <Hero />
     </section>
-    <section id="Services">Parallax</section>
+    <section id="Services"><Parallax type="services" /></section>
     <section>Services</section>
-    <section id="Portfolio">Parallax</section>
-    <section>Portfolio1</section>
-    <section>Portfolio2</section>
-    <section>Portfolio3</section>
+    <section id="Portfolio"><Parallax type="portfolio" /></section>
+    <Portfolio />
     <section id="Contact">Contact</section>
   </div>;
 }
---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,64 @@
+import { useRef } from "react"
+import "./Portfolio.scss"
+import { motion, useScroll, useSpring } from "framer-motion"
+
+const items = [
+    {
+        id: 1,
+        title: "React Commerce",
+        img: "https://images.pexels.com/photos/36717/amazing-animal-beautiful-beautifull.jpg?auto=compress&cs=tinysrgb&w=600",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum minima harum dolor, ducimus perspiciatis laudantium maxime itaque alias error nesciunt!"
+    },
+    {
+        id: 2,
+        title: "Next.JS Commerce",
+        img: "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum minima harum dolor, ducimus perspiciatis laudantium maxime itaque alias error nesciunt!"
+    },
+    {
+        id: 3,
+        title: "Vanilla JS App",
+        img: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum minima harum dolor, ducimus perspiciatis laudantium maxime itaque alias error nesciunt!"
+    },
+    {
+        id: 4,
+        title: "Java API Rest",
+        img: "https://images.pexels.com/photos/325185/pexels-photo-325185.jpeg?auto=compress&cs=tinysrgb&w=600",
+        description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum minima harum dolor, ducimus perspiciatis laudantium maxime itaque alias error nesciunt!"
+    }
+]
+
+    const Single = ({item}) => {
+        return (
+        <section>
+            {item.title}
+        </section>)
+    }
+
+export function Portfolio() {
+
+    const ref = useRef()
+
+    const {scrollYProgress} = useScroll({
+        target: ref, 
+        offset: ["end end", "start start"]
+    })
+
+    const scaleX = useSpring(scrollYProgress, {
+        stiffness: 100,
+        damping: 30
+    })
+
+    return <div className="portfolio" ref={ref}>
+        <div className="progress">
+            <h1>Featured Works</h1>
+            <motion.div style={{ scaleX }} className="progressBar"></motion.div>
+        </div>
+        {items.map(item => (
+            <Single item={item} key={item.id}/>
+        ))}
+    </div>
+}
+
+export default Portfolio
